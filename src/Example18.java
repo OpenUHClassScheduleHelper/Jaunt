cc371515ee2263055fa7e2a40231155e3d840261
Rob Namahoe <rnamahoe@hawaii.edu>
2014-04-07 10:30:26 -1000

    Initial commit
    Example and Jar file included.

---
+++ src/Example18.java
@@ -0,0 +1,20 @@
+import com.jaunt.*;
+import com.jaunt.component.*;
+
+public class Example18{
+  public static void main(String[] args){
+    try{
+      UserAgent userAgent = new UserAgent(); 
+      userAgent.visit("https://www.sis.hawaii.edu/uhdad/avail.classes?i=MAN&t=201430&s=ICS");
+      Table table = userAgent.doc.getTable("<table class= listOfClasses>");
+      
+      System.out.println("\nColumn having 'Course'");
+      Elements elements = table.getCol("Course");
+      for(Element element : elements) System.out.println(element.getText());
+      
+    }
+    catch(JauntException e){
+      System.err.println(e);
+    }
+  }
+}
