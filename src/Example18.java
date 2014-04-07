342489f3b1ebc64bec2682f38f456515884b21a5
Rob Namahoe <rnamahoe@hawaii.edu>
2014-04-07 11:29:24 -1000

    minor edit

--- src/Example18.java
+++ src/Example18.java
@@ -8,9 +8,12 @@ public class Example18{
       userAgent.visit("https://www.sis.hawaii.edu/uhdad/avail.classes?i=MAN&t=201430&s=ICS");
       Table table = userAgent.doc.getTable("<table class= listOfClasses>");
       
-      System.out.println("\nColumn having 'Course'");
-      Elements elements = table.getCol("Course");
-      for(Element element : elements) System.out.println(element.getText());
+      System.out.println("\nColumn having 'Time'");
+      Elements elements = table.getCol("Time");
+      for (Element element : elements) {
+        //System.out.println();
+        System.out.println(element.getText());
+      }
       
     }
     catch(JauntException e){
